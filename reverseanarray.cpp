b9a2ad3f8ca8578257a83c2bc26761efadcb521e
mariam786 <M9ghani@gmail.com>
2025-06-20 21:41:44 +0530

    reverse an array basic codez

---
+++ reverseanarray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // Input array size
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    // Input array elements
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    // Output the reversed array
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
